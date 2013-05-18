cd48590b00b2d5b45fdd91ac5de41597e44ec7ee
Benjamin Woodruff <benjaminwoodruff@ufl.edu>
2013-05-17 21:59:18 -0400

    Port html template from Less to Scss with Compass
    There's even less code, and the resulting CSS is more efficient (thanks
    to the `@extend` directive)

--- templates/html/processor.py
+++ templates/html/processor.py
@@ -7,25 +7,21 @@ from lib import base_processor
 def process(config, template_path, output_path, data):
     data = base_processor.preprocess(data, "html")
     base_processor.process(config, template_path, output_path, data)
-    
+
     loader = jinja2.FileSystemLoader(os.path.join(template_path, "data"))
     environment = jinja2.Environment(loader=loader)
-    
+
     for template in config["use_templates"]:
         with open(os.path.join(output_path, template), "w") as f:
             f.write(environment.get_template(template).render(data))
-    
-    for f in config["less_files"]:
-        subprocess.check_call(
-            [
-                "lessc", os.path.join(template_path, "data", f),
-                os.path.join(output_path, f.replace(".less", ".css"))
-            ],
-            stdin=sys.stdin, stdout=sys.stdout, stderr=sys.stderr,
-        )
-    
+
     subprocess.check_call(
-        "yui-compressor -o *.css *.css",
-        stdin=sys.stdin, stdout=sys.stdout, stderr=sys.stderr, shell=True,
-        cwd=output_path
+        [
+            "compass", "compile",
+            "--output-style=expanded",
+            "--relative-assets",
+            "--css-dir", output_path
+        ] + config["sass_files"],
+        cwd=os.path.join(template_path, "data"),
+        stdin=sys.stdin, stdout=sys.stdout, stderr=sys.stderr,
     )
